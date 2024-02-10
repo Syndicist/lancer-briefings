a54c0b639809225c3976f752eea5b954c2dddc76
Tyler Roman <31106886+Syndicist@users.noreply.github.com>
2024-02-09 21:29:18 -0500

    added hercynia

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -15,7 +15,7 @@
     </div>
     <div class="rhombus">&nbsp;</div>
     <video autoplay muted loop width="90px" height="90px">
-      <source src="/planet.webm" type="video/webm" />
+      <source src="/hercynia.webp" type="video/webp" />
     </video>
     <div class="location-info">
       <div class="location-row" id="planet-year">
