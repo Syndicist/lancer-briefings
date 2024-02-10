ea3d452e7e88f840f0da5844346ff7a5f3642910
Tyler Roman <31106886+Syndicist@users.noreply.github.com>
2024-02-10 13:35:18 -0500

    Fixed spelling mistakes

--- src/App.vue
+++ src/App.vue
@@ -117,7 +117,7 @@ export default {
           "mech": "Better Than One"
         },
         {
-          "callsign": "Lechesis",
+          "callsign": "Lachesis",
           "alias": "Ambroz Cristales",
           "code": "7cd700cc-c990-48ed-892f-e5468de724c4///NDL-C-DEEP-STATION//a98c3e28-ad4a-4f89-bcd9-501464e960da",
           "corpro": "GMS",
