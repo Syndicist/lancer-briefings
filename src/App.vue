707c4e0b184ac386d0e69763490210095fa109df
Tyler Roman <31106886+Syndicist@users.noreply.github.com>
2024-02-10 13:33:22 -0500

    added lechesis

--- src/App.vue
+++ src/App.vue
@@ -109,12 +109,20 @@ export default {
           "mech": "We All Love Pino"
         },
         {
-          "callsign": "Cachivache",
+          "callsign": "Catch",
           "alias": "Federico Volorov",
           "code": "7cd700cc-c990-48ed-892f-e5468de724c4///NDL-C-DEEP-STATION//a98c3e28-ad4a-4f89-bcd9-501464e960da",
           "corpro": "GMS",
           "frame": "Everest",
           "mech": "Better Than One"
+        },
+        {
+          "callsign": "Lechesis",
+          "alias": "Ambroz Cristales",
+          "code": "7cd700cc-c990-48ed-892f-e5468de724c4///NDL-C-DEEP-STATION//a98c3e28-ad4a-4f89-bcd9-501464e960da",
+          "corpro": "GMS",
+          "frame": "Everest",
+          "mech": "DAETHSHEAD"
         },
       ],
       "header": {
